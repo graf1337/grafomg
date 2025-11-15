a02d5fc4643541ab6a3f5e4a04f67cd1638115d1
graf1337 <t3mamarchenko@gmail.com>
2025-11-15 23:54:26 +0300

    Create task1.java

---
+++ LR_1/task1.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+ArrayList<String> li = new ArrayList<>(List.of("x", "1", "y", "2", "z", "3"));  // список (массивная структура)
+
+Stack<String> stack = new Stack<>();  // стек
+stack.push("x");  // добавление в стек
+stack.push("y");
+stack.push("z");
